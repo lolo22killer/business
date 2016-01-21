99704aaf894203150cd66dec3f931766240b91ea
Danil Kolpakov <kolpakov.danil@gmail.com>
2016-01-21 16:29:34 +0600

    First project

---
+++ app/js/vendor/script.js
@@ -0,0 +1,3 @@
+//= ../../../bower_components/jquery/dist/jquery.js
+//= ../../../bower_components/jquery-validation/dist/jquery.validate.js
+//= ../../../bower_components/masonry/dist/masonry.pkgd.js
