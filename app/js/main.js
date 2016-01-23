f6c16da5775f128abecb991959cd79a891c3b30d
Danil Kolpakov <kolpakov.danil@gmail.com>
2016-01-23 12:25:21 +0600

    Validate form

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,59 @@
- $(function () {
+$(function () {
 
     /*===========================
-       #
+       Валидация форм
    ===========================*/
-   // = partials/name.js
 
+   $('#form1').validate({
+   	rules: {
+   		name: {
+				required: true
+   		},
+			tel:{
+				required: true,
+				digits: true,
+				rangelength: [10, 10]
+    	},
+    	"company-name":{
+    		required: true
+    	}
+    },
+    messages:{
+    	name: {
+				required: "Введите ваше имя"
+   		},
+			tel:{
+				required: "Введите ваш номер телефона",
+				digits: "Неправильно набран номер",
+				rangelength: "Введите десятизначный номер телефона"
+    	},
+    	"company-name":{
+    		required: "Введите название вашей компонии"
+    	}
+    },
+   	errorPlacement: function(error, element) {
+      error.insertBefore(element);
+  	}
 
+  });	// !validate
+
+  $('#form2').validate({
+  	rules: {
+   		email: {
+				required: true,
+				email: true
+   		}
+    },
+    messages:{
+    	email: {
+				required: "Введите ваш email",
+				email: "Введеный email не коректен"
+   		}
+    },
+   	errorPlacement: function(error, element) {
+      error.insertBefore(element);
+  	}
+  });
 
 }); // !Jquery
 
